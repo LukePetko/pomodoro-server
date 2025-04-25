fb19a7078bb72d79b93173837734be6feb943bdb
Lukáš Peťko <luke.petko@gmail.com>
2025-04-25 23:19:07 +0200

    adding env files

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,19 @@ import (
 	config "github.com/lukepetko/pomodoro-server/internal/config"
 	mqtt "github.com/lukepetko/pomodoro-server/internal/mqtt"
 	"github.com/lukepetko/pomodoro-server/internal/timer"
+    "github.com/joho/godotenv"
 )
 
 
 
 func main() {
     fmt.Println("MQTT Time Tracker app started!")
+
+    err := godotenv.Load()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
     
     if err := mqtt.Init(); err != nil {
         fmt.Println(err)
